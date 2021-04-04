c857ab93a9604468c2a0a3b3e682667b746fc88d
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-04-05 00:28:11 +0200

    Change styling details

--- Base64EncoderDecoder.Wpf/MainWindow.xaml.cs
+++ Base64EncoderDecoder.Wpf/MainWindow.xaml.cs
@@ -13,35 +13,35 @@ namespace Base64EncoderDecoderWpf
 
         private void encode_Click_1(object sender, RoutedEventArgs e)
         {
-            if (inputTextBox.Text.Length >= 1)
+            if (InputTextBox.Text.Length >= 1)
             {
-                var processedText = new ProcessedTextModel {InputText = inputTextBox.Text};
+                var processedText = new ProcessedTextModel {InputText = InputTextBox.Text};
                 processedText.OutputText =
                     Base64EncoderDecoderCore.Base64EncoderDecoder.ConvertTextToBase64(processedText.InputText);
-                outputTextBox.Text = processedText.OutputText;
+                OutputTextBox.Text = processedText.OutputText;
             }
         }
 
         private void decode_Click(object sender, RoutedEventArgs e)
         {
-            if (inputTextBox.Text.Length >= 1)
+            if (InputTextBox.Text.Length >= 1)
             {
-                var processedText = new ProcessedTextModel {InputText = inputTextBox.Text};
+                var processedText = new ProcessedTextModel {InputText = InputTextBox.Text};
                 processedText.OutputText =
                     Base64EncoderDecoderCore.Base64EncoderDecoder.ConvertTextFromBase64(processedText.InputText);
                 if (processedText.OutputText == "")
                 {
-                    outputTextBox.Text = "Invalid input - the length of the input is not a multiple of 4.";
+                    OutputTextBox.Text = "Invalid input - the length of the input is not a multiple of 4.";
                 }
 
-                outputTextBox.Text = processedText.OutputText;
+                OutputTextBox.Text = processedText.OutputText;
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            inputTextBox.Text = "";
-            outputTextBox.Text = "";
+            InputTextBox.Text = "";
+            OutputTextBox.Text = "";
         }
 
         private void enter_KeyDown(object sender, KeyEventArgs e)
