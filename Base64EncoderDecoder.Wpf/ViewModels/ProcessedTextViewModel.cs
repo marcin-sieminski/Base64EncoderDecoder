cebfefc49856d6811d4ee31710332c6f85530720
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-04-11 00:48:37 +0200

    Refactor implement the MVVM pattern

--- Base64EncoderDecoder.Wpf/ViewModels/ProcessedTextViewModel.cs
+++ Base64EncoderDecoder.Wpf/ViewModels/ProcessedTextViewModel.cs
@@ -1,8 +1,63 @@
-﻿namespace Base64EncoderDecoderWpf.ViewModels
+﻿using Base64EncoderDecoderWpf.Annotations;
+using Base64EncoderDecoderWpf.Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Base64EncoderDecoderWpf.ViewModels
 {
-    public class ProcessedTextViewModel
+    public class ProcessedTextViewModel : INotifyPropertyChanged
     {
-        public string InputText { get; set; }
-        public string OutputText { get; set; }
+        private ProcessedTextModel _model = new ProcessedTextModel();
+
+        public string InputText
+        {
+            get => _model.InputText;
+            set
+            {
+                _model.InputText = value;
+                _model.OutputText = Base64EncoderDecoderCore.Base64EncoderDecoder.ConvertTextToBase64(_model.InputText);
+                OnPropertyChanged(nameof(InputText));
+            }
+        }
+
+        public string OutputText
+        {
+            get => _model.OutputText;
+            set
+            {
+                _model.OutputText = value;
+                OnPropertyChanged(nameof(OutputText));
+            }
+        }
+
+        private ICommand _clear = null;
+
+        public ICommand Clear
+        {
+            get
+            {
+                if (_clear == null)
+                {
+                    _clear = new RelayCommand(
+                        (object o) =>
+                        {
+                            _model.Clear();
+                            OnPropertyChanged(nameof(InputText));
+                        },
+                        (object o) => _model.InputText != null && _model.InputText.Length > 0);
+                }
+
+                return _clear;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ Base64EncoderDecoder.Wpf/ViewModels/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+public class RelayCommand : ICommand
+{
+    private Action<object> _execute;
+    private Func<object, bool> _canExecute;
+
+    public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (_canExecute == null)
+        {
+            return true;
+        }
+
+        return _canExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add
+        {
+            CommandManager.RequerySuggested += value;
+        }
+        remove
+        {
+            CommandManager.RequerySuggested -= value;
+        }
+    }
+}
