bfef0634820818c16cb1bd3781f25309e236fc86
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-04-16 10:19:52 +0200

    Add close window with esc button feature

---
+++ Base64EncoderDecoder.Wpf/Behaviors/CloseWindowButtonBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Base64EncoderDecoderWpf.Behaviors
+{
+    public class CloseWindowButtonBehavior : Behavior<Window>
+    {
+        public Key KeyBehavior { get; set; }
+
+        protected override void OnAttached()
+        {
+            Window window = AssociatedObject;
+            if(window != null) window.PreviewKeyDown += window_PreviewKeyDown;
+        }
+
+        private void window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Window window = (Window)sender;
+            if (e.Key == KeyBehavior) window.Close();
+        }
+    }
+}
